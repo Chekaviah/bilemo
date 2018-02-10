3fd44b28ab40a622e7fb5db517c79f28a941d5c7
Chekaviah <chekaviah@gmail.com>
2018-02-10 13:35:02 +0100

    add datafixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Client;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -35,10 +36,59 @@ class UserFixtures extends Fixture implements ContainerAwareInterface
         $user = new User();
         $user->setUsername('user');
         $user->setEmail('user@website.net');
-        $user->setPassword($passwordEncoder->encodePassword($admin, 'user'));
+        $user->setPassword($passwordEncoder->encodePassword($user, 'user'));
         $user->setActive(true);
         $manager->persist($user);
 
+        $clients = $this->getClients();
+
+        foreach ($this->getUsers() as $u) {
+            $reseller = new User();
+            $reseller->setUsername($u[1]);
+            $reseller->setEmail($u[2]);
+            $reseller->setPassword($passwordEncoder->encodePassword($reseller, 'user'));
+            $reseller->setActive(true);
+            $manager->persist($reseller);
+
+            for ($i=$u[0]; $i<100; $i+=10) {
+                $client = new Client();
+                $client->setName($clients[$i][0]);
+                $client->setEmail($clients[$i][1]);
+                $client->setAddress($clients[$i][2]);
+                $client->setUser($reseller);
+
+                $manager->persist($client);
+            }
+        }
+
         $manager->flush();
+    }
+
+    private function getUsers(): array
+    {
+        $users = [];
+        for ($i=0; $i<10; $i++) {
+            $users[] = [
+                $i,
+                'reseller-'.$i,
+                'reseller'.$i.'@website.net'
+            ];
+        }
+
+        return $users;
+    }
+
+    private function getClients(): array
+    {
+        $clients = [];
+        for ($i=1; $i<=100; $i++) {
+            $clients[] = [
+                'client-'.$i,
+                'client'.$i.'@website.net',
+                $i.' rue de l\'infini',
+            ];
+        }
+
+        return $clients;
     }
 }
