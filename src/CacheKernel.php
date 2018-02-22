6b7a5996d8c40a41021c0799ae989c3e27532719
Chekaviah <chekaviah@gmail.com>
2018-02-22 12:05:37 +0100

    enable http cache and optimize autoloader

---
+++ src/CacheKernel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
+
+/**
+ * Class CacheKernel
+ *
+ * @author Mathieu GUILLEMINOT <guilleminotm@gmail.com>
+ */
+class CacheKernel extends HttpCache
+{
+
+}
